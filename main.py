e726c24cb16768cc521c22fb3147b51a8a4a584a
MikroMirza <mirkodjukic314@gmail.com>
2025-10-15 13:17:50 +0200

    [FIX] quick fix in data representation

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from Preprocess import Preprocessor
 from KNNTagRecommender import KNNTagRecommender
 
@@ -7,4 +9,16 @@ if __name__ == "__main__":
 
     knnRecommender = KNNTagRecommender(5, 0.3)
     knnRecommender.fit(pp.user_item_matrix, pp.user_tagged)
-    print(knnRecommender.recommend(2))
+    recommendations = knnRecommender.recommend(2)
+
+    rec_df = recommendations.reset_index()
+    rec_df.columns = ["artistID", "score"]
+    rec_df["artistID"] = rec_df["artistID"].astype(int)
+
+    # Clean artist IDs
+    pp.artists["id"] = pd.to_numeric(pp.artists["id"], errors="coerce")
+    pp.artists = pp.artists.dropna(subset=["id"])
+    pp.artists["id"] = pp.artists["id"].astype(int)
+
+    rec_df = rec_df.merge(pp.artists, left_on="artistID", right_on="id", how="left")
+    print(rec_df[["artistID", "name", "score"]])
